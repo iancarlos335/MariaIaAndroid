077895f89e00d3ce0edccf594e9c9db8305d3590
Ian Carlos Azevedo Martins <iancarlos335@gmail.com>
2024-01-28 22:06:48 -0300

    latest updates

--- app/src/main/java/com/example/mariaiaandroid/MainActivity.kt
+++ app/src/main/java/com/example/mariaiaandroid/MainActivity.kt
@@ -4,45 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mariaiaandroid.ui.theme.MariaIaAndroidTheme
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.generationConfig
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-
-
+        val config = generationConfig {
+            temperature = 0.9f
+        }
         val generativeModel = GenerativeModel(
             modelName = "gemini-pro",
             apiKey = BuildConfig.apiKey,
-            generationConfig =
+            generationConfig = config
 
         )
 
         setContent {
             MariaIaAndroidTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-
-                }
-
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MyForm(
+                        name = "Gemini",
+                        modifier = Modifier.padding(innerPadding),
+                        value = "teste",
+                        trial = { input ->
+                            print(input)
+                        }
                     )
                 }
             }
@@ -51,17 +52,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyForm(
+    name: String,
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChanged: (String) -> Unit
+) {
+    val initialValue = rememberUpdatedState(value)
+
+    Column {
+        Text(
+            text = name,
+            modifier = modifier
+        )
+
+        TextField(
+            value = initialValue.value,
+            onValueChange = { initialValue.value = it },
+            placeholder = { Text("Insira Algo") }
+        )
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                text = "Enviar",
+                modifier = modifier
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyFormPreview() {
     MariaIaAndroidTheme {
-        Greeting("Android")
+        MyForm("Insira Algo", Modifier.fillMaxSize()) { input -> print(input) }
+
     }
 }
